a815a0447e2dad8014534fc29bd8b76949100ac0
YegorDovydenko <dovydenko.e93@gmail.com>
2026-08-24 15:02:25 +0300

    add github actions for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/frontend-lead-plan/" : "/",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
     port: 5556,
     open: true,
   },
-});
+}));
